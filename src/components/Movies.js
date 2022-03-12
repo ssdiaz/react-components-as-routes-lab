e1ea893a5797cb92a8bfbbe562a9f8a23098634b
Samantha Diaz <samidiaz3@gmail.com>
2022-03-12 16:15:26 -0500

    commit tree 59b27044aab33cf91b67ae23b577800f99ff1a74

--- src/components/Movies.js
+++ src/components/Movies.js
@@ -4,7 +4,20 @@ import { movies } from '../data';
 const Movies = () => {
   return (
     <div>
-        {/*{code here}*/}
+        <h1>Movies Pages</h1>
+
+        {movies.map((movie, index) => (
+          <div key={index}>
+            <h3>Title: {movie.title}</h3>
+            <h3>Time: {movie.time}</h3>
+            <h3>Genres:</h3>
+              <ul>
+                {movie.genres.map((genre, index) => (
+                  <li key={index}>{genre}</li>
+                ))}
+              </ul>
+          </div>
+        ))}
     </div>
   );
 };
